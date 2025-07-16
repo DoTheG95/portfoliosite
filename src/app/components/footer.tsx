51bbe330e883941bfa6c9a79eca5568967e04376
Gary <gary.kim.1009@gmail.com>
2025-07-16 17:10:22 +1000

    update tcg back

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div className="flex flex-row gap-6 items-center justify-center font-[family-name:var(--font-geist-sans)]">
       <a
         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="/Gary Kim CV 2025.pdf"
+        href="/GaryKimCV.pdf"
         download
       >
         <Image
