e17a2dc0cbc28eba56afa0376f9b038a67fe1cae
Gary Kim <gary.kim.1009@gmail.com>
2025-07-17 12:55:21 +1000

    updated site url for SkyWorld

--- src/app/components/workexperience.tsx
+++ src/app/components/workexperience.tsx
@@ -169,7 +169,7 @@ export default function WorkExperience() {
                   <li>
                      <a
                      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                     href="https://worldpodcasts.com/"
+                     href="https://www.skyworld.co.nz/"
                      target="_blank"
                      rel="noopener noreferrer"
                      >
